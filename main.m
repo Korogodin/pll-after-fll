6a4f4eebe511e6aeb4811adc29a387a7b5ea3947
vvdneprov@gmail.com <vvdneprov@gmail.com@2fc0e6e1-251e-a07d-d5c9-6ebd67d2f4e2>
2013-04-11 12:45:49 +0000

    мучения с ЧД

--- main.m
+++ main.m
@@ -6,29 +6,30 @@ global K
 
 Tmod = 30; % Время моделирования
 Tf = 0.001; % Период работы фильтров
-Tc = 0.001; % Период интегрирования в корреляторе
+Tc = 0.0005; % Период интегрирования в корреляторе
 K = fix(Tmod/Tc); % Число интервалов накопления в корреляторе за время моделирования
 stdn_IQ = 8; % СКО шума квадратурных сумм
 
 Ff = [1  Tf
-    0  1]; % Переходная матрица для модели частоты (в темпе фильтра)
+      0  1]; % Переходная матрица для модели частоты (в темпе фильтра)
 
 Fp = [1 Tf 0
-    0 1  Tf
-    0 0  1]; % Переходная матрица для модели фазы (в темпе фильтра)
+      0 1  Tf
+      0 0  1]; % Переходная матрица для модели фазы (в темпе фильтра)
 
 Fc = [1 Tc 0
-    0 1  Tc
-    0 0  1]; % Переходная матрица для модели фазы
+      0 1  Tc
+      0 0  1]; % Переходная матрица для модели фазы
 
 Fincorr = [1 Tc
-    0 1]; % Переходная матрица набега фазы в корреляторе
+           0 1]; % Переходная матрица набега фазы в корреляторе
 
-HPLL = 45; % Hz, полоса ФАП
-CKO_W_PLL = nan(1,length(HPLL));
-CKO_W_FLL = nan(1,length(HPLL));
-CKO_Phi_PLL = nan(1,length(HPLL));
-for j = 1:length(HPLL)
+std_a = [10:10:100];
+CKO_W_PLL = nan(1,length(std_a));
+CKO_W_FLL = nan(1,length(std_a));
+CKO_Phi_PLL = nan(1,length(std_a));
+for j = 1:length(std_a)
+    fprintf('SKZ = %.0f\n',std_a(j))
     MemoryAlloc; % Резервирование памяти
     
     qcno_ist = 45; % SNR на каждом интервале, дБГц
@@ -44,27 +45,29 @@ for j = 1:length(HPLL)
     
     % Расчет параметров формирующего шума. GLONASS, page 162
     alpha = 0.1; % Ширина спектра ускорения, с^-1
-    std_a = 40; %СКЗ ускорения
-    S_ksi = 2*(33*std_a)^2 * alpha; %Спектральная плотность формирующего шума
-    stdIst = sqrt(S_ksi * Tc); %СКО формирующего шума
+%     std_a = 40; %СКЗ ускорения
+    S_ksi = 2*(33*std_a(j))^2 * alpha; %Спектральная плотность формирующего шума
+    stdIst = sqrt(S_ksi * Tf); %СКО формирующего шума
     nIst = randn(1,K);
     
     % Расчет СКО эквивалентных шумов
     stdEqNoise = stdn_IQ/A_IQ;
+    stdEqNoiseW = sqrt((1/(qcno*Tc^3))*(1+1/(2*qcno*Tc)));
     
+    CKO_W_teor(j) = (4*S_ksi*(Tf*stdEqNoiseW^2)^3)^(1/8) / (2*pi);
     
     % ЧАП
-    HFLL = 5.5; % Hz, полоса ЧАП
+%     HFLL = 5.5; % Hz, полоса ЧАП
     KFLL = nan(2,1); % Вектор-столбец коэффициентов фильтра
-    KFLL(2) = 2*16/9*HFLL^2; % Коэффициенты непрерывной системы в установившемся режиме
+    KFLL(2) = (stdIst/(stdEqNoiseW*Tf));
+%     KFLL(2) = 2*16/9*HFLL^2; % Коэффициенты непрерывной системы в установившемся режиме
     KFLL(1) = sqrt(2*KFLL(2));
     KFLL = KFLL*Tf; % Переход к коэффициентам дискретной системы
     
     % ФАП
-    %     fprintf('Polosa = %.1f\n', HPLL(j));
     KPLL = nan(3,1); % Вектор-столбец коэффициентов фильтра
-%     KPLL(3) = (1.2*HPLL(j))^3; % Коэффициенты непрерывной системы в установившемся режиме
-    KPLL(3) = (stdIst/stdEqNoise);
+    %     KPLL(3) = (1.2*HPLL(j))^3; % Коэффициенты непрерывной системы в установившемся режиме
+    KPLL(3) = (stdIst/(stdEqNoise*Tf));
     KPLL(2) = 2*(KPLL(3))^(2/3);
     KPLL(1) = 2*(KPLL(3))^(1/3);
     KPLL = KPLL*Tf; % Переход к коэффициентам дискретной системы
@@ -73,6 +76,7 @@ for j = 1:length(HPLL)
     nQ = 1*stdn_IQ.*randn(1,K); % Q-comp noise
     
     w = 0; Isum = 0; Qsum = 0; Iold = 1; Qold = 0;
+    Ik1 = 1; Qk1 = 0;
     for k = 1:K
         
         % Расчет стат.эквивалентов корреляционных сумм
@@ -91,16 +95,16 @@ for j = 1:length(HPLL)
         
         w = w + 1;
         if w == fix(Tf/Tc)
-            
+            Ik2 = I(k); Qk2 = Q(k);
             % Фазовый дискриминатор
-            UdPLL(k) = -(I(k)*sin((XextrPLL(2) - Xcorr(2))*Tf/2*1 + (XextrPLL(1) - Xcorr(1))) + Q(k)*cos((XextrPLL(2) - Xcorr(2))*Tf/2*1 + (XextrPLL(1) - Xcorr(1))));
+            UdPLL(k) = -(Isum*sin((XextrPLL(2) - Xcorr(2))*Tf/2*1 + (XextrPLL(1) - Xcorr(1))) + Qsum*cos((XextrPLL(2) - Xcorr(2))*Tf/2*1 + (XextrPLL(1) - Xcorr(1))));
             UdPLL_mean(k) = A_IQ*sinc((Xist(2) - Xcorr(2))*Tf/2 /pi)*sin((Xist(2) - XextrPLL(2))*Tf/2*1 + (Xist(1) - XextrPLL(1)));
             SdPLL = A_IQ; % Крутизна ФД
             XestPLL = XextrPLL + KPLL*UdPLL(k)/SdPLL;  % Вектор оценок на очередной интервал
             XextrPLL = Fp*XestPLL;                % Экстраполяция на следующий интервал
             
-            UdFLL(k) = (I(k)*Qold - Q(k)*Iold); % Частотный дискриминатор
-            SdFLL = Tc*(A_IQ*Tf/Tc)^2 * 1.3; % Крутизна ЧД
+            UdFLL(k) = (Ik2*Qk1 - Qk2*Ik1); % Частотный дискриминатор
+            SdFLL = Tc * A_IQ^2; % Крутизна ЧД
             XestFLL = XextrFLL + KFLL*UdFLL(k)/SdFLL;  % Вектор оценок на очередной интервал фильтра
             XextrFLL = Ff*XestFLL;             % Экстраполяция на следующий интервал
             
@@ -108,6 +112,9 @@ for j = 1:length(HPLL)
             w = 0;
             Iold = Isum; Isum = 0;
             Qold = Qsum; Qsum = 0;
+            Ik1 = Ik2; Qk1 = Qk2;
+        elseif w == 1
+            Ik1 = I(k); Qk1 = Q(k);
         end
         
         Err_W_FLL(k) = Xist(2) - XestFLL(1);
@@ -129,16 +136,9 @@ D_etta_phi = mean((UdPLL - UdPLL_mean).^2); % флуктуационная характеристика диск
 
 t = (1:K)*Tc;
 
-hF = 0;
-hF = figure(hF + 1);
-subplot(2,1,1)
-plot(t, Err_W_PLL /2 /pi, t, Err_W_FLL /2 /pi);
+plot(t, Err_W_FLL /2 /pi);
 title('Freq error ')
 ylabel('\deltaf, Hz');
-xlabel('t, s');
-subplot(2,1,2)
-plot(t, Err_Phi_PLL /pi * 180);
-ylabel('\delta\phi, deg');
 xlabel('t, s');
 
 % figure(2)
