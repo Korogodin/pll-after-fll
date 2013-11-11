48f26cd9f100cea3c67a3ab338ae63af97f76edd
vvdneprov@gmail.com <vvdneprov@gmail.com@2fc0e6e1-251e-a07d-d5c9-6ebd67d2f4e2>
2013-11-11 20:11:58 +0000

    На пути к успеху. Сегодня получено выражение для дискриминационной характеристики sign(I)*Q (см. файл дискриминатор с усреднением по НС) это выражение промоделировано в файле testing_sign.

---
+++ trunk/Фазовый дискриминатор/matlab/testing_sign.m
@@ -0,0 +1,51 @@
+close all
+
+stdnIQ = 8;
+qcno_dB = 35;
+qcno = 10^(qcno_dB/10);
+T = 5e-3;
+Aiq = stdnIQ*sqrt(2*T*qcno);
+
+Np = 10000;
+
+
+
+deltaPhiOporn = [-pi:pi/180*5:pi];
+Nphi = length(deltaPhiOporn);
+
+udPlus = zeros(1,Nphi);
+udMinus = zeros(1,Nphi);
+
+Pimore = zeros(1,Nphi);
+Piless = zeros(1,Nphi);
+
+for i = 1:Nphi
+         
+    mI = Aiq*cos(deltaPhiOporn(i));
+    mQ = -Aiq*sin(deltaPhiOporn(i));
+    
+    nI = stdnIQ*randn(1,Np);
+    nQ = stdnIQ*randn(1,Np);
+    
+    Pimore(i) = 0.5 + 0.5*erf(mI/(stdnIQ*sqrt(2)));
+    Piless(i) = 0.5 - 0.5*erf(mI/(stdnIQ*sqrt(2)));
+    
+    for k = 1:Np
+        
+        Iplus = mI + nI(k);
+        Qplus = mQ + nQ(k);
+        
+        Iminus = -mI + nI(k);
+        Qminus = -mQ + nQ(k);
+        
+        udPlus(i) = udPlus(i) - sign(Iplus)*Qplus;
+        udMinus(i) = udMinus(i) - sign(Iminus)*Qminus;
+        
+    end
+end
+udPlus = udPlus/Np;
+udMinus = udMinus/Np;
+
+
+plot(deltaPhiOporn*180/pi, [udPlus; udMinus; 0.5*(udPlus + udMinus); Aiq*(Pimore - Piless).*sin(deltaPhiOporn)]);
+
